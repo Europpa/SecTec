45e0d8a1dc4171af059434189ccb3486186917a7
Antogol <antonio.css07@gmail.com>
2015-11-25 01:01:16 -0600

    se modifico index y hoja de estilo

--- Views/index/index.php
+++ Views/index/index.php
@@ -9,59 +9,51 @@
 <div class="container">
 	<div class="row">
 	    <br>
-		<div class="col-sm-offset-4 col-md-4">
-			<ul class="nav nav-tabs" role="tablist">
-				<li role="presentation"><a href="#alumno" aria-controls="alumno" role="tab" data-toggle="tab">Portal del Alumno</a></li>
-				<li role="presentation"><a href="#administradores" aria-controls="administradores" data-toggle="tab">Portal Administradores</a></li>
-			</ul>
-			<div class="tab-content">
-				<div role="tabpanel" class="tab-pane fade in active" id="alumno">
-				    <br>	
-					<h4 class="text-center">Acceso AlumnoSS</h4>
-					<br>
-					<form class="form-horizontal">
-						<div class="form-group">
-					    	<label for="inputmatricula3" class="col-sm-3 control-label">Matricula</label>
-					    	<div class="col-sm-9">
-					      		<input type="text" class="form-control" id="inputmatricula3" name="matricula" placeholder="Matricula">
-					    	</div>
-					  	</div>
-					  	<div class="form-group">
-					    	<label for="inputContraseña3" class="col-sm-3 control-label">Contraseña</label>
-					    	<div class="col-sm-9">
-					      		<input type="password" class="form-control" id="inputContraseña3" name="contraseña" placeholder="Contraseña">
-					    	</div>
-					  	</div>
-					<div class="form-group">
-					    <div class="col-sm-offset-3 col-sm-9">
-					    	<button class="btn btn-lg btn-primary btn-block" type="submit">Entrar</button>
-					    </div>
-					</div>
-					</form>
-				</div>
-				<div role="tabpanel" class="tab-pane fade" id="administradores">	
-				    <br>	
-					<h4 class="text-center">Acceso Administradores</h4>
-					<br>
-					<form class="form-horizontal">
-						<div class="form-group">
-					    	<label for="inputmatricula3" class="col-sm-3 control-label">Matricula</label>
-					    	<div class="col-sm-9">
-					      		<input type="text" class="form-control" id="inputmatricula3" name="matricula" placeholder="Matricula">
-					    	</div>
-					  	</div>
-					  	<div class="form-group">
-					    	<label for="inputContraseña3" class="col-sm-3 control-label">Contraseña</label>
-					    	<div class="col-sm-9">
-					      		<input type="password" class="form-control" id="inputContraseña3" name="contraseña" placeholder="Contraseña">
-					    	</div>
-					  	</div>
-					    <div class="form-group">
-					        <div class="col-sm-offset-3 col-sm-9">
-					    	    <button class="btn btn-lg btn-primary btn-block" type="submit">Entrar</button>
-					        </div>
-					    </div>
-					</form>
+		<div class="col-sm-offset-6 col-md-5">
+		    <div class="panel panel-default">
+                <div class="panel-body">
+			        <ul class="nav nav-tabs" role="tablist">
+				        <li role="presentation"><a href="#administradores" aria-controls="administradores"  data-toggle="tab">Portal Administador</a></li>
+				        <li role="presentation"><a href="#alumno" aria-controls="alumno" data-toggle="tab">Portal Alumno</a></li>
+			        </ul>
+			        <div class="tab-content">
+				        <div role="tabpanel" class="tab-pane fade in active" id="administradores">
+				           <br>	
+					       <h4 class="text-center">Acceso Administradores</h4>
+					       <br>
+					        <form class="form-horizontal">
+						        <div class="form-group">
+					    	        <label for="inputMatricula" class="sr-only">Matricula</label>
+					      		    <input type="text" id="inputMatricula" class="form-control" placeholder="Matricula" required="" autofocus="">
+					  	        </div>
+					  	        <div class="form-group">
+					  	        	<label for="inputContraseña3" class="sr-only">Contraseña</label>
+					      		    <input type="password" id="inputcontraseña" class="form-control" placeholder="Contraseña" required="" autofocus="">					 
+					  	        </div>
+					            <div class="form-group">
+					    	        <button class="btn btn-lg btn-primary btn-block" type="submit">Entrar</button>
+					            </div>
+					        </form>
+				            </div>
+				            <div role="tabpanel" class="tab-pane fade" id="alumno">	
+				            <br>	
+					        <h4 class="text-center">Acceso Alumno</h4>
+					        <br>
+					        <form class="form-horizontal">
+						        <div class="form-group">
+					    	        <label for="inputMatricula" class="sr-only">Matricula</label>
+					      		    <input type="text" id="inputMatricula" class="form-control" placeholder="Matricula" required="" autofocus="">
+					        	</div>
+					  	        <div class="form-group">
+					    	        <label for="inputContraseña3" class="sr-only">Contraseña</label>
+					      		    <input type="password" id="inputcontraseña" class="form-control" placeholder="Contraseña" required="" autofocus="">
+					  	        </div>
+					            <div class="form-group">
+					    	        <button class="btn btn-lg btn-primary btn-block" type="submit">Entrar</button>
+					            </div>
+					        </form>
+					    </br>   
+					</br>     
 				</div>
 			</div>
 		</div>	
@@ -69,9 +61,9 @@
 </div>
 </body>
 <!--LINKEA TODOS LOS CSS-->
-<link rel="stylesheet" href="<?php echo BASE_URL.'Views'.DS.'css'.DS.'estiloslogin.css';?>" type="text/css">
 <link rel="stylesheet" href="<?php echo BASE_URL.'Views'.DS.'css'.DS.'bootstrap.min.css';?>" type="text/css">
 <link rel="stylesheet" href="<?php echo BASE_URL.'Views'.DS.'css'.DS.'bootstrap.theme.css';?>" type="text/css">
+<link rel="stylesheet" href="<?php echo BASE_URL.'Views'.DS.'css'.DS.'estiloslogin.css';?>" type="text/css">
 <script src="<?php echo BASE_URL.'Views'.DS.'js'.DS.'jquery-2.1.4.min.js';?>"></script>
 <script src="<?php echo BASE_URL.'Views'.DS.'js'.DS.'bootstrap.min.js';?>"></script>
 </html>
